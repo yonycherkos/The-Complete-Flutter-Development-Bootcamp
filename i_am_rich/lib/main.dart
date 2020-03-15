44eb29604e5c052d4143458ebf1885f8f6bbb1e8
yonycherkos <yonycherkos@gmail.com>
2020-03-15 14:18:15 +0300

    section-3.1 create new flutter project from scratch

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// the main function is an entry point for all flutter app
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("hello world!"),
+      ),
+    ),
+  );
+}

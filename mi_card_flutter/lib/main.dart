a7cbdcb575c0ac7c042a97deb14935aef661adce
yonycherkos <yonycherkos@gmail.com>
2020-03-16 21:53:39 +0300

    section-6.3 How to Use Container Widgets

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -10,7 +10,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            child: Text('Hello World!'),
+            width: 100.0,
+            height: 100.0,
+            margin: EdgeInsets.all(25.0),
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+          ),
+        ),
       ),
     );
   }

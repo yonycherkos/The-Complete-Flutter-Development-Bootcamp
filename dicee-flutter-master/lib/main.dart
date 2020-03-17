ef70e63e49b745f52eb7c356bb641f8616feb3bf
yonycherkos <yonycherkos@gmail.com>
2020-03-17 16:58:46 +0300

    section-7.3 How to Use Intention Actions

--- dicee-flutter-master/lib/main.dart
+++ dicee-flutter-master/lib/main.dart
@@ -18,19 +18,23 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image(
-            image: AssetImage('images/dice1.png'),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
           ),
-        ),
-        Expanded(
-          child: Image(
-            image: AssetImage('images/dice1.png'),
-          ),
-        ),
-      ],
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }

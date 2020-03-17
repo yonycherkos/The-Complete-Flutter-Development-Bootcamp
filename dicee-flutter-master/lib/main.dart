23f804e0dbc3557594915761115f061366c88b1e
yonycherkos <yonycherkos@gmail.com>
2020-03-17 16:57:41 +0300

    section-7.2 Using the Expanded Widget to Create Flexible Layouts

--- dicee-flutter-master/lib/main.dart
+++ dicee-flutter-master/lib/main.dart
@@ -18,6 +18,19 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
+        ),
+      ],
+    );
   }
 }

f4d36bd1c32b1933a66dc74faa567784cf5d225d
yonycherkos <yonycherkos@gmail.com>
2020-03-18 14:24:41 +0300

    section-9.2. What are Flutter & Dart Packages

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,8 +8,11 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.lightBlue,
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Text(nouns.first),
+          ),
         ),
       ),
     );

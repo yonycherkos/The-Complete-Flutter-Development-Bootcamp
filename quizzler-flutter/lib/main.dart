5a1c9374c5a84fd73d8dd3fc384cb4c1c695ee01
yonycherkos <yonycherkos@gmail.com>
2020-03-20 11:26:56 +0300

    section-10.8. Creating a Question Class

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,13 +27,13 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+
+  List<Question> questions = [
+    Question(question: 'You can lead a cow down stairs but not up stairs.', answer: false),
+    Question(question: 'Approximately one quarter of human bones are in the feet.', answer: true),
+    Question(question: 'A slug\'s blood is green.', answer: true)
   ];
 
-  List<bool> answers = [false, true, true];
 
   int questionNumber = 0;
 
@@ -48,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,7 +74,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = questions[questionNumber].questionAnswer;
                 setState(() {
                   if(correctAnswer == true) {
                     scoreKeeper.add(
@@ -110,7 +111,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = questions[questionNumber].questionAnswer;
                 setState(() {
                   if(correctAnswer == false) {
                     scoreKeeper.add(
---
+++ quizzler-flutter/lib/question.dart
@@ -0,0 +1,8 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+  Question({String question, bool answer}) {
+    questionText = question;
+    questionAnswer = answer;
+  }
+}

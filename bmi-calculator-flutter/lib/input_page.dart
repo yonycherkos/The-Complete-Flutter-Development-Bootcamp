c120b1ab320ea1d51b86542def37a2d02da4d8c2
yonycherkos <yonycherkos@gmail.com>
2020-04-01 09:01:43 +0300

    section-12.2. How to Use Flutter Themes

---
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: Theme(
+        data: ThemeData(accentColor: Colors.purple),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}

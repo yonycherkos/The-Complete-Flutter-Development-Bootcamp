8a4c8660dbc72ed8f685e7202900c852d50a99c8
yonycherkos <yonycherkos@gmail.com>
2020-04-04 15:50:55 +0300

    section-13.9. JSON Parsing and Dynamic Types

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -18,7 +19,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getData() async {
     http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
     if (response.statusCode == 200) {
-      print(response.body);
+      String data = response.body;
+
+      var temprature = jsonDecode(data)['main']['temp'];
+      var conditionId = jsonDecode(data)['weather'][0]['id'];
+      var cityName = jsonDecode(data)['name'];
+
+      print('tempratur: $temprature');
+      print('conditionId: $conditionId');
+      print('cityName: $cityName');
+      
     } else {
       print(response.statusCode);
     }

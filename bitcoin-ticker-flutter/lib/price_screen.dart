c4dc1cc18e36d2c548b52e5f8474e8a55f260b7f
yonycherkos <yonycherkos@gmail.com>
2020-04-08 14:13:19 +0300

    section-14.2. The Material DropdownButton Widget

--- bitcoin-ticker-flutter/lib/price_screen.dart
+++ bitcoin-ticker-flutter/lib/price_screen.dart
@@ -6,6 +6,8 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrancy = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +44,27 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton(
+                value: selectedCurrancy,
+                items: [
+                  DropdownMenuItem(
+                    child: Text('USD'),
+                    value: 'USD',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('BRL'),
+                    value: 'BRL',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('CAD'),
+                    value: 'CAD',
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    selectedCurrancy = value;
+                  });
+                }),
           ),
         ],
       ),

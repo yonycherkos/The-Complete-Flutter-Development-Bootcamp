aa00cf13ef91b1c00b9e9fcfef471243078a643a
yonycherkos <yonycherkos@gmail.com>
2020-04-25 11:21:39 +0300

    section-15.18. Saving Data into Cloud Firestore

--- flash-chat-flutter/lib/screens/chat_screen.dart
+++ flash-chat-flutter/lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
@@ -11,12 +12,15 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
+  final _firestore = Firestore.instance;
+  FirebaseUser loggedInUser;
+  String messageText;
 
   void getCurrentUser() async {
     try {
-      FirebaseUser currentUser = await _auth.currentUser();
-      if(currentUser != null) {
-        print(currentUser.email);
+      loggedInUser = await _auth.currentUser();
+      if(loggedInUser != null) {
+        print(loggedInUser.email);
       }
     } catch(e) {
       print(e);
@@ -57,14 +61,17 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      _firestore.collection('messages').add({
+                        'text': messageText,
+                        'sender': loggedInUser.email,
+                      });
                     },
                     child: Text(
                       'Send',
